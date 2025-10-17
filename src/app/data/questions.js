fad2da53afff1b61b0fbfce0835324969e698980
F15 <duongquocthai368@gmail.com>
2025-10-18 00:14:25 +0700

    feat: add question and mutiple choice quiz which is simple

---
+++ src/app/data/questions.js
@@ -0,0 +1,17 @@
+export const questions = [
+  {
+    question: "What is the capital of Japan?",
+    options: ["Seoul", "Tokyo", "Beijing", "Bangkok"],
+    answer: "Tokyo",
+  },
+  {
+    question: "Which language does React use?",
+    options: ["Python", "C++", "JavaScript", "PHP"],
+    answer: "JavaScript",
+  },
+  {
+    question: "Next.js is built on top of which library?",
+    options: ["Vue", "React", "Angular", "Svelte"],
+    answer: "React",
+  },
+];
